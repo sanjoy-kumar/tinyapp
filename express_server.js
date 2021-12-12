0ba1113b4ec2e22d8f0360a057f4b479ad11caf1
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2021-12-12 02:30:23 -0500

    Redirect Short URLs

--- express_server.js
+++ express_server.js
@@ -31,7 +31,7 @@ const generateRandomString = () => {
 
 
 
-// ------------------- Add Route ----------------------------------------
+// ------------------- Add GET Route ----------------------------------------
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -46,6 +46,14 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+
 
 
 // ------------------ Add POST Route --------------------------------
