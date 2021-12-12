96f360e73a55249248881321631f0817824e8afd
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2021-12-12 02:11:34 -0500

    Adding a Second Route and show Template

--- express_server.js
+++ express_server.js
@@ -14,26 +14,17 @@ const urlDatabase = {
 
 
 // ------------------- Add Route ----------------------------------------
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 
 
